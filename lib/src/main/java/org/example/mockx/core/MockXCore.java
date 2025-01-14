6a3ee534f20a3305cbeeee2cef4d7b92e103fb2d
Subodh Singh <singh.01.subodh@gmail.com>
2025-01-14 14:34:44 +0530

    Added Mocking class for better state management
    Signed-off-by: Subodh Singh <singh.01.subodh@gmail.com>

--- lib/src/main/java/org/example/mockx/core/MockXCore.java
+++ lib/src/main/java/org/example/mockx/core/MockXCore.java
@@ -10,22 +10,20 @@ import org.example.mockx.core.behavior.Behavior;
 import org.example.mockx.core.behavior.ReturnBehavior;
 import org.example.mockx.core.behavior.ThrowBehavior;
 
-import java.lang.reflect.Method;
 import java.util.*;
 
 public class MockXCore {
-    private int nextMockId = 0;
-    private final ArrayList<Invocation> invocations = new ArrayList<>();
-    private final HashMap<Integer, HashMap<Method, List<Map.Entry<Object[], Behavior>>>> drb = new HashMap<>();
+    private final List<Mocking> mockingList = new ArrayList<>();
+    private Invocation lastInvocation = null;
 
     public synchronized <T> T createMock(Class<T> typeToMock) {
-        int mockId = nextMockId++;
-        drb.put(mockId, new HashMap<>());
-        MockInvocationHandler invocationHandler = new MockInvocationHandler(mockId, this);
+        int mockId = mockingList.size();
+        Mocking mocking = new Mocking(mockId, this);
+        mockingList.add(mocking);
         Class<? extends T> subClass = new ByteBuddy()
             .subclass(typeToMock, ConstructorStrategy.Default.NO_CONSTRUCTORS)
             .method(ElementMatchers.any())
-            .intercept(InvocationHandlerAdapter.of(invocationHandler))
+            .intercept(InvocationHandlerAdapter.of(mocking))
             .make()
             .load(getClass().getClassLoader())
             .getLoaded();
@@ -33,54 +31,43 @@ public class MockXCore {
     }
 
     public synchronized <T> Stubbing<T> when(T methodCall) {
-        if (invocations.isEmpty()) {
-            throw new IllegalStateException("Stub-able method invocation not found on any mock!");
+        if (lastInvocation == null) {
+            throw new IllegalStateException("Method Invocation Missing");
         }
-        Invocation invocation = invocations.get(invocations.size() - 1);
-        return new Stubbing<>(
-            invocation,
+        Stubbing<T> stubbing = new Stubbing<>(
+            lastInvocation,
             this
         );
+        lastInvocation = null;
+        return stubbing;
     }
 
     synchronized void doReturn(Invocation invocation, Object returnValue) {
-        if (!drb.get(invocation.mockId).containsKey(invocation.method)) {
-            drb.get(invocation.mockId).put(invocation.method, new ArrayList<>());
-        }
-        List<Map.Entry<Object[], Behavior>> list = drb.get(invocation.mockId).get(invocation.method);
-        list.add(new AbstractMap.SimpleEntry<>(invocation.args, new ReturnBehavior(returnValue)));
+        // TODO check if type of return values matches the method
+        mockingList.get(invocation.mockId).addBehavior(invocation, new ReturnBehavior(returnValue));
     }
 
     synchronized void doThrow(Invocation invocation, Throwable throwable) {
-        if (!drb.get(invocation.mockId).containsKey(invocation.method)) {
-            drb.get(invocation.mockId).put(invocation.method, new ArrayList<>());
-        }
-        List<Map.Entry<Object[], Behavior>> list = drb.get(invocation.mockId).get(invocation.method);
-        list.add(new AbstractMap.SimpleEntry<>(invocation.args, new ThrowBehavior(throwable)));
+        mockingList.get(invocation.mockId).addBehavior(invocation, new ThrowBehavior(throwable));
     }
 
-    // Invocation Handling
     public synchronized Behavior handleInvocation(Invocation invocation) {
         Behavior stubbedBehavior = resolveBehavior(invocation);
-        invocations.add(invocation);
+        lastInvocation = invocation;
         return stubbedBehavior;
     }
 
     private synchronized Behavior resolveBehavior(Invocation invocation) {
-        List<Map.Entry<Object[], Behavior>> list = drb.get(invocation.mockId).getOrDefault(invocation.method, Collections.emptyList());
-        // Match the most recent stubbed behavior
-        for (int i = list.size() - 1; i >= 0; i--) {
-            if (argsMatch(invocation.args, list.get(i).getKey())) {
-                return list.get(i).getValue();
-            }
+        Behavior resolvedBehavior = mockingList.get(invocation.mockId).findMatchingBehavior(invocation);
+        if (resolvedBehavior == null) {
+            resolvedBehavior = new ReturnBehavior(DefaultValues.get(invocation.method.getReturnType()));
         }
-        // fallback to default
-        return new ReturnBehavior(DefaultValues.get(invocation.method.getReturnType()));
+        return resolvedBehavior;
     }
 
     // Note:
     // - args[] is null if the method was invoked without any argument
-    private boolean argsMatch(Object[] args1, Object[] args2) {
+    static boolean argsMatch(Object[] args1, Object[] args2) {
         if (args1 == args2) return true;                        // returns when both null
         if (args1 == null || args2 == null) return false;       // returns when either null
         for (int i = 0; i < args1.length; i++) {
---
+++ lib/src/main/java/org/example/mockx/core/Mocking.java
@@ -0,0 +1,46 @@
+package org.example.mockx.core;
+
+import org.example.mockx.core.behavior.Behavior;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.*;
+
+public class Mocking implements InvocationHandler {
+    private final int mockId;
+    private final MockXCore core;
+    private final HashMap<Method, List<Map.Entry<Object[], Behavior>>> stubbedBehavior = new HashMap<>();
+
+    public Mocking(int mockId, MockXCore core) {
+        this.mockId = mockId;
+        this.core = core;
+    }
+
+    public void addBehavior(Invocation invocation, Behavior behavior) {
+        if (!stubbedBehavior.containsKey(invocation.method)) {
+            stubbedBehavior.put(invocation.method, new ArrayList<>());
+        }
+        List<Map.Entry<Object[], Behavior>> list = stubbedBehavior.get(invocation.method);
+        list.add(new AbstractMap.SimpleImmutableEntry<>(invocation.args, behavior));
+    }
+
+    public Behavior findMatchingBehavior(Invocation invocation) {
+        // Optimistic optimization: not checking if mockId matches
+        List<Map.Entry<Object[], Behavior>> list = stubbedBehavior.getOrDefault(invocation.method, Collections.emptyList());
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (MockXCore.argsMatch(invocation.args, list.get(i).getKey()))
+                return list.get(i).getValue();
+        }
+        return null;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Behavior stubbedBehavior = core.handleInvocation(new Invocation(
+            this.mockId,
+            method,
+            args
+        ));
+        return stubbedBehavior.run();
+    }
+}
